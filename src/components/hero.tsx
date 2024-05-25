a6558862daf9507a3f5066efc0bfbfdaaa815774
Tiffany Hernández <tiffale2013@gmail.com>
2024-05-24 22:59:43 -0600

    Responsive inicio
    Modificaciones en el hero de la pagina de inicio para que sea responsive

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,4 +1,4 @@
-import type {StaticImageData} from "next/image";
+import type { StaticImageData } from "next/image";
 import Image from "next/image";
 
 interface HeroProps {
@@ -12,14 +12,14 @@ export default function Hero(props: HeroProps) {
     <div className="relative h-screen">
      <div className="absolute -z-10 inset-0">
       <Image
-        src = {props.imgData}
-        alt = {props.imgAlt}
-        fill
-        style = {{ objectFit: 'cover'}} 
+        src={props.imgData}
+        alt={props.imgAlt}
+        layout="fill"
+        objectFit="cover"
       />
      </div>
-     <div className="pt-48 flex justify-center items-center z-20">
-       <h1 className="text-white text-6xl">{props.title}</h1>
+     <div className="pt-12 md:pt-48 flex justify-center items-center z-20">
+       <h1 className="text-white text-4xl md:text-6xl text-center">{props.title}</h1>
      </div>
      <div className="absolute inset-0 bg-gradient-to-r from-[#24262b]"></div>
     </div>
