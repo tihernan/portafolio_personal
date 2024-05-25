d5c8857add420d14356419807d99a66a053e90e0
Tiffany Hernández <tiffale2013@gmail.com>
2024-05-24 23:14:09 -0600

    texto inicio
    Alineación del texto del inicio según responsive

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -16,11 +16,10 @@ export default function Hero(props: HeroProps) {
         alt={props.imgAlt}
         layout="fill"
         objectFit="cover"
-        className="object-center object-cover lg:object-top"
       />
      </div>
-     <div className="pt-12 md:pt-48 flex justify-center items-center z-20">
-       <h1 className="text-white text-4xl md:text-6xl text-center">{props.title}</h1>
+     <div className="pt-25 md:pt-48 flex justify-center items-center z-20">
+       <h1 className="text-white text-4xl md:text-6xl text-center align-">{props.title}</h1>
      </div>
      <div className="absolute inset-0 bg-gradient-to-r from-[#24262b]"></div>
     </div>
