0759f427c9582bb1de35d3b3668e078e7d4b0043
Tiffany Hernández <tiffale2013@gmail.com>
2024-05-24 23:21:51 -0600

    Header y hero
    Aumento del margen izquierdo del logo en el header responsive
    Aumento del margen superior del texto en el hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -19,7 +19,7 @@ export default function Hero(props: HeroProps) {
       />
      </div>
      <div className="pt-12 md:pt-48 flex justify-center items-center z-20">
-       <h1 className="text-white text-4xl md:text-6xl text-center align-middle mb-4">{props.title}</h1>
+       <h1 className="text-white text-4xl md:text-6xl text-center align-middle mb-8">{props.title}</h1>
      </div>
      <div className="absolute inset-0 bg-gradient-to-r from-[#24262b]"></div>
     </div>
